bd5fb7af4773f274c054c6617a7020ad8cc73906
johmert <johmert@users.noreply.github.com>
2022-01-03 01:32:51 -0800

    practice stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
+function WelcomeBack(props) {
+  return <p>Welcome back, {props.name}</p>
+}
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const name = "Jessica";
+  const url = "https://johmert.github.io/portfolio";
+  const linkText = "your portfolio"
+  const element = (
+    <p>
+      {" "}
+      Please visit <a href={url}>{linkText}</a>.
+    </p>
+  );
+  return (
+    <>
+    <WelcomeBack name="Jessica"/>
+    {element}
+    </>
+    );
 }
 
 export default App;
